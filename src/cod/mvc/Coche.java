8e22cff97e5c8dd86290186e5c8306841e4cfcb3
Anx0Fdez <anxofernandezrodriguez05@gmail.com>
2024-05-03 09:51:09 +0200

    creando constructor coche

--- src/cod/mvc/Coche.java
+++ src/cod/mvc/Coche.java
@@ -4,5 +4,9 @@ public class Coche {
     private String matricula;
     private String modelo;
     private int velocidad;
-
+    public Coche(String matricula, String modelo, int velocidad) {
+        this.matricula = matricula;
+        this.modelo = modelo;
+        this.velocidad = velocidad;
+    }
 }
