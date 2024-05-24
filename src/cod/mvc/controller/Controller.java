6333c2170eaf9be08ae3a8071b6a0c5871cf7c1d
Anx0Fdez <anxofernandezrodriguez05@gmail.com>
2024-05-24 09:50:18 +0200

    miModel

--- src/cod/mvc/controller/Controller.java
+++ src/cod/mvc/controller/Controller.java
@@ -8,4 +8,17 @@ public class Controller {
 
     private final Model miModel;
 
+    public Controller(Model miModel){
+        this.miModel = miModel;
+
+        // instanciamos al observador de la velocidad
+        ObsVelocidad observoVelocidad = new ObsVelocidad();
+        miModel.addObserver(observoVelocidad);
+
+        // instanciamos un segundo observador para el limite de velocidad
+        ObsVelocidad observoLimite = new ObsVelocidad();
+        miModel.addObserver(observoLimite);
+    }
+
+
 }
