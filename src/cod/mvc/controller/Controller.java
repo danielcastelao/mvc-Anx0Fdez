d6588d57ce3f9798479597b1f5736cad1b39ea62
Anx0Fdez <anxofernandezrodriguez05@gmail.com>
2024-05-24 09:40:39 +0200

    controller update

--- src/cod/mvc/controller/Controller.java
+++ src/cod/mvc/controller/Controller.java
@@ -1,9 +1,15 @@
 package cod.mvc.controller;
 
+import cod.mvc.model.Coche;
 import cod.mvc.model.Model;
 import cod.mvc.view.View;
 
-public class Controller extends ObsVelocidad {
+public class Controller implements Observer {
 
     private ObsVelocidad observaVelocidad;
+
+    @Override
+    public void update(Coche coche) {
+
+    }
 }
