826c60a677b9664d01c53ced8ae4e14311dcc062
Anx0Fdez <anxofernandezrodriguez05@gmail.com>
2024-05-27 09:49:22 +0200

    update view

--- src/cod/mvc/view/View.java
+++ src/cod/mvc/view/View.java
@@ -1,8 +1,8 @@
 package cod.mvc.view;
 
 public class View {
-    public static void muestraVelocidad(String matricula, int velocidad) {
-        System.out.println("El coche con matrícula " + matricula + " tiene una velocidad de " + velocidad + " km/h.");
-
+    public static boolean muestraVelocidad(String matricula, Integer v){
+        System.out.println("[View] " + matricula + ": " + v + "km/hr");
+        return true;
     }
 }
