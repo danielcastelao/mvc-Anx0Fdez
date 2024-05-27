519b57164f46c76ddbefe094880e75c96423e110
Anx0Fdez <anxofernandezrodriguez05@gmail.com>
2024-05-27 09:48:08 +0200

    update coche.java

--- src/cod/mvc/model/Coche.java
+++ src/cod/mvc/model/Coche.java
@@ -3,11 +3,11 @@ package cod.mvc.model;
 public class Coche {
     public String matricula;
     public String modelo;
-    public int velocidad;
-    public Coche(String matricula, String modelo) {
-        this.matricula = matricula;
+    public Integer velocidad;
+
+    public Coche(String modelo, String matricula) {
         this.modelo = modelo;
+        this.matricula = matricula;
         this.velocidad = 0;
     }
-
 }
