869cbe697d6d415c7ee0957e16cf298509879fb4
Anx0Fdez <anxofernandezrodriguez05@gmail.com>
2024-05-30 23:39:48 +0200

    closes #6

--- test/ControllerTest.java
+++ test/ControllerTest.java
@@ -10,18 +10,28 @@ import java.io.PrintStream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+/**
+ * Esta clase contiene pruebas unitarias para la clase Controller.
+ */
 public class ControllerTest {
     private Controller controller;
     private Model model;
 
+    /**
+     * Este método configura los objetos necesarios para las pruebas.
+     * Se ejecuta antes de cada prueba.
+     */
     @BeforeEach
     public void setup() {
         model = new Model();
         controller = new Controller(model);
     }
 
+    /**
+     * Esta prueba verifica si se crea un coche con el nombre y matrícula dados.
+     */
     @Test
-    public void createsCarWithGivenNameAndMatricula() {
+    public void creaCocheConNombreYMatriculaDados() {
         controller.crearCoche("TestCar", "1234");
         Coche coche = model.getCoche("1234");
         assertNotNull(coche);
@@ -29,19 +39,25 @@ public class ControllerTest {
         assertEquals("1234", coche.matricula);
     }
 
+    /**
+     * Esta prueba verifica si la velocidad del coche cambia correctamente.
+     */
     @Test
-    public void changesCarSpeed() {
+    public void cambiaVelocidadDelCoche() {
         controller.crearCoche("TestCar", "1234");
         controller.cambiarVelocidad("1234", 50);
         Coche coche = model.getCoche("1234");
         assertEquals(50, coche.velocidad);
     }
 
-    // test cambiar velocidad y supera el limite de 120
+    /**
+     * Esta prueba verifica si la velocidad del coche cambia correctamente y si excede el límite.
+     * También verifica si se imprime la salida correcta cuando se excede el límite de velocidad.
+     */
     @Test
-    public void changesCarSpeedAndExceedsLimit() {
+    public void cambiaVelocidadDelCocheYExcedeLimite() {
 
-        // Redirigir la salida estándar
+        // Redirige la salida estándar
         ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
 
@@ -50,11 +66,9 @@ public class ControllerTest {
         Coche coche = model.getCoche("BX5555");
         assertEquals(150, coche.velocidad);
 
-        // Comprobar la salida
+        // Verifica la salida
         String salidaEsperada = "[ObserverVelocidad] Se ha cambiado la velocidad: 150km/hr\n" +
                 "[View] BX5555: 150km/hr\n" + "[ObserverLimite] INFRACCION\n";
         assertEquals(salidaEsperada, outContent.toString());
-
     }
-
 }
