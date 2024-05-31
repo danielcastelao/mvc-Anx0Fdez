2723840f4bad2eb44aa3487cbb827c92d26ce970
Anx0Fdez <anxofernandezrodriguez05@gmail.com>
2024-05-31 09:23:13 +0200

    metodo mostrar coche

--- src/cod/mvc/view/View.java
+++ src/cod/mvc/view/View.java
@@ -1,8 +1,17 @@
 package cod.mvc.view;
 
+import cod.mvc.model.Coche;
+
+import java.util.Scanner;
+
 public class View {
     public static boolean muestraVelocidad(String matricula, Integer v){
         System.out.println("[View] " + matricula + ": " + v + "km/hr");
         return true;
     }
+
+    public static void mostrarCoche(Coche coche) {
+        System.out.println("[View] Modelo: " + coche.modelo + ", Matricula: " + coche.matricula + ", Velocidad: " + coche.velocidad + "km/hr");
+    }
+
 }
