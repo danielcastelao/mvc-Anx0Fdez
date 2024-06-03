cf934817b10ce3918422884e203a7a82a435a7c0
Anx0Fdez <anxofernandezrodriguez05@gmail.com>
2024-06-03 09:33:17 +0200

    test crear coche

---
+++ test/ModelTest.java
@@ -0,0 +1,26 @@
+import cod.mvc.controller.Observer;
+import cod.mvc.model.Coche;
+import cod.mvc.model.Model;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ModelTest {
+    private Model model;
+    private Coche coche;
+
+    @BeforeEach
+    public void setup() {
+        model = Model.getInstance();
+        coche = model.crearCoche("Modelo1", "ABC123");
+    }
+
+    @Test
+    public void crearCoche_createsNewCar() {
+        Coche newCoche = model.crearCoche("Modelo2", "DEF456");
+        assertNotNull(newCoche);
+        assertEquals("Modelo2", newCoche.modelo);
+        assertEquals("DEF456", newCoche.matricula);
+    }
+
+}
